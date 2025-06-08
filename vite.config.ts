db82a3c8abd97f3d16512890897a981c0051dc1b
Joon Hao <52413727+bjh-developer@users.noreply.github.com>
2025-06-08 20:02:08 +0800

    Enable vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import { resolve } from 'path';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, 'index.html'),
+//       },
+//     },
+//   },
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   }
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
