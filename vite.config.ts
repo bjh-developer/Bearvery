9ff37e73af4597f784e66a0ab630d9f338a520dd
Joon Hao <52413727+bjh-developer@users.noreply.github.com>
2025-06-08 20:53:54 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // ✅ use relative paths for assets
   plugins: [react()],
 })
