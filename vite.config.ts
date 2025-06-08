d08df43f29e47c5bc087d2fd5bfe625df26ed6db
Joon Hao <52413727+bjh-developer@users.noreply.github.com>
2025-06-08 21:19:24 +0800

    Improved vercel settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: './', // ✅ use relative paths for assets
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
